348d2e02f87585106c0b001d680f3bffda742ab9
Zeio Nara <zeionara@gmail.com>
2025-12-16 00:35:12 +0300

    feat(create-bucket): added command for bucket creation

--- s3/__main__.py
+++ s3/__main__.py
@@ -15,25 +15,29 @@ def main():
     pass
 
 
-# @main.command()
-# @argument('name', type = str)
-# def create_bucket(name: str):
-#     endpoint = getenv('CLOUD_RU_S3_ENDPOINT')
-#
-#     key = getenv('CLOUD_RU_S3_TENANT_ID') + ':' + getenv('CLOUD_RU_S3_KEY_ID')
-#     secret = getenv('CLOUD_RU_S3_KEY_SECRET')
-#     region = getenv('CLOUD_RU_S3_REGION')
-#     service = getenv('CLOUD_RU_S3_SERVICE')
-#
-#     auth = AWS4Auth(key, secret, region, service)
-#
-#     response = put(
-#         f'{endpoint}/{name}',
-#         auth = auth,
-#         timeout = TIMEOUT
-#     )
-#
-#     print(response.text)
+@main.command()
+@argument('name', type = str)
+def create_bucket(name: str):
+    endpoint = 'https://s3.cloud.ru'
+
+    key = getenv('CLOUD_RU_S3_TENANT_ID') + ':' + getenv('CLOUD_RU_S3_KEY_ID')
+    secret = getenv('CLOUD_RU_S3_KEY_SECRET')
+    region = getenv('CLOUD_RU_S3_REGION')
+    service = getenv('CLOUD_RU_S3_SERVICE')
+
+    auth = AWS4Auth(key, secret, region, service)
+
+    response = put(
+        f'{endpoint}/{name}',
+        auth = auth,
+        timeout = TIMEOUT
+    )
+
+    if response.status_code == SUCCESS:
+        print(f'Created bucket {name}')
+    else:
+        print(f'Failed to create bucket {name}')
+        print(f'Error: {response.text}')
 
 
 @main.command()
@@ -62,6 +66,9 @@ def push(source: str, destination: str):
     if response.status_code == SUCCESS:
         print('Upload successful. Check out your file:')
         print(f'wget {endpoint}/{destination}')
+    else:
+        print(f'Failed to upload file as {destination}')
+        print(f'Error: {response.text}')
 
 
 if __name__ == '__main__':
